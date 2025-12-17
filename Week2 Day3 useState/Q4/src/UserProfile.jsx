d27f193716ce3c1812e6fdb28eccb9ca25b682a3
suprajagangarajula7 <suprajagangarajula7@gmail.com>
2025-12-17 13:47:15 +0530

    passing props

---
+++ Week2 Day3 useState/Q4/src/UserProfile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import UserInfo from "./UserInfo";
+
+function UserProfile() {
+  const name = "Lara";
+  const age = 22;
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <UserInfo name={name} age={age} />
+    </div>
+  );
+}
+
+export default UserProfile;
